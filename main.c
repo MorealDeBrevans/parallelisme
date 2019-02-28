4c2fb1999d86be1c3a9664f29563c12b81d9f080
Elise Jacquemet <elise.jacquemet@insa-lyon.fr>
2019-02-28 09:11:22 +0100

    modified Q2 and added Q3

--- main.c
+++ main.c
@@ -3,19 +3,47 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(int argc, char *argv[]) {
-	double* vecteur;
-	vecteur=malloc(1024*sizeof(double));
-	double* vecteur2;
-	vecteur2=malloc(1024*sizeof(double));
-	
-	srand(time(NULL));
-	
-	for(int i=0;i<1024;i++) vecteur[i]= ( (double)rand() * 100 ) / (double)RAND_MAX;
-	for(int i=0;i<1024;i++) vecteur2[i]= ( (double)rand() * 100 ) / (double)RAND_MAX;
-	
-	for(int i=0;i<1024;i++) printf("%.2f\n", vecteur[i]);
-	
-	return 0;
+void remplir(double * vecteur, int taille){
+	for(int i=0;i<taille;i++) vecteur[i]= ( (double)rand() * 100 ) / (double)RAND_MAX;
 }
 
+void afficher(double * vecteur, int taille){
+	printf("[ ");
+	for(int i=0;i<taille-1;i++) printf("%.2f, ",vecteur[i]);
+	printf("%.2f ]\n", vecteur[taille-1]);
+}
+
+double * ajouter(double * vecteur1, double * vecteur2, int taille){
+	double * resultat;
+	resultat = malloc(taille*sizeof(double));
+	for(int i=0;i<taille;i++) resultat[i] = vecteur1[i] + vecteur2[i];
+	return resultat;
+}
+
+int main(int argc, char *argv[]) {
+
+	int taille = atoi(argv[1]);
+	srand(time(NULL));
+
+	double* vecteur;
+	vecteur=malloc(taille*sizeof(double));
+	double* vecteur2;
+	vecteur2=malloc(taille*sizeof(double));
+	
+	/* Q1. Remplissage aleatoire des vecteurs */
+	remplir(vecteur, taille);
+	remplir(vecteur2, taille);
+	
+	/* Q2. Affichage des vecteurs */	
+	afficher(vecteur, taille);
+	afficher(vecteur2, taille);
+
+	/* Q3. Addition de deux vecteurs */	
+	double * vecteur3 = ajouter(vecteur, vecteur2, taille);
+	afficher(vecteur3, taille);
+	
+	free(vecteur);
+	free(vecteur2);
+	free(vecteur3);
+	return 0;
+}
