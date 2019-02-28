9b5a7cf60fa9fcb6f5316577f811cde66f71a64c
tmorealdeb <theophile.moreal-de-brevans@insa-lyon.fr>
2019-02-28 08:54:15 +0100

    re

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 int main(int argc, char *argv[]) {
-	printf("hello wurst\n");
+	double* vecteur;
+	vecteur=malloc(1024*sizeof(double));
+	double* vecteur2;
+	vecteur2=malloc(1024*sizeof(double));
+	
+	srand(time(NULL));
+	
+	for(int i=0;i<1024;i++) vecteur[i]= ( (double)rand() * 100 ) / (double)RAND_MAX;
+	for(int i=0;i<1024;i++) vecteur2[i]= ( (double)rand() * 100 ) / (double)RAND_MAX;
+	
+	for(int i=0;i<1024;i++) printf("%.2f\n", vecteur[i]);
+	
 	return 0;
 }
 
